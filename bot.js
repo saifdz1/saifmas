cd8afb688937688c619451c78afe0d7f3c656ea3
saifdz1 <40006650+saifdz1@users.noreply.github.com>
2018-07-11 14:27:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const prefix = '+'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
+client.user.setGame(`+help | +inv`,"http://twitch.tv/SaifDz")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
